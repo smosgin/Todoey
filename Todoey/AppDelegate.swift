6c52eff7414bcf28fc36c6933620b4fb1f57e073
Seth <smo1@umbc.edu>
2018-07-14 18:42:06 -0400

    Remove unnecessary CoreData code in AppDelegate

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -7,18 +7,16 @@
 //
 
 import UIKit
-import CoreData
 import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-//    var realm: Realm?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        print(Realm.Configuration.defaultConfiguration.fileURL)
+//        print(Realm.Configuration.defaultConfiguration.fileURL)
         
         let config = Realm.Configuration(
             // Set the new schema version. This must be greater than the previously used
@@ -29,7 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             // a schema version lower than the one set above
             migrationBlock: { migration, oldSchemaVersion in
                 // We haven’t migrated anything yet, so oldSchemaVersion == 0
-                if (oldSchemaVersion < 0) {
+                if (oldSchemaVersion < 1) {
                     print("Entered migration")
                     migration.enumerateObjects(ofType: Item.className(), { (oldObject, newObject) in
                         newObject!["dateCreated"] = Date()
@@ -42,52 +40,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         Realm.Configuration.defaultConfiguration = config
         
         do {
-            let realm = try Realm()
+            _ = try Realm()
             print("Realm created/accessed in appdidfinishlaunching")
         } catch {
             print("Error initializing new realm \(error)")
         }
         
-        
         return true
     }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-//        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "APP'S GONNA GO BYEBYE"), object: nil)
-//        let myNavController = self.window?.rootViewController as! UINavigationController
-//        let myViewControllers = myNavController.viewControllers
-//        myViewController.saveData()
-        self.saveContext()
-    }
-    
-    // MARK: - Core Data stack
-    
-    // Lazy variable gets memory allocated when it is needed as opposed to prepared beforehand
-    lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "DataModel") // name matches datamodel
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-    
-    // MARK: - Core Data Saving support
-    
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
-
-
 }
 
